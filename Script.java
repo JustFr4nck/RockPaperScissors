6b8a78da40a4f559c4b26aee9113660fe773e006
JustFr4nck <francyperry07@gmail.com>
2025-08-22 23:23:56 +0200

    game version 1.0.0

--- Script.java
+++ Script.java
@@ -3,57 +3,81 @@ import java.util.Scanner;
 
 public class Script {
 
-   
+    // MAIN
 
     public static void main(String[] args) {
-        
+
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        String name, choose;
+        String name, choose, bot;
+        int ran;
 
-        while(true){
+        while (true) {
 
-            System.out.println("Inserisci il tuo nome: ");
+            System.out.println("\nInserisci il tuo nome: ");
             name = scanner.nextLine();
 
-            if(isAlpha(name)){
+            if (isAlpha(name)) {
 
                 break;
             }
 
-            System.out.println("ERRORE: Inserire nome senza valori numerici");
+            System.out.println("\nERRORE: Inserire nome senza valori numerici");
 
         }
 
-         System.out.println("\n___INIZIO_GIOCO___");
+        while (true) {
 
-        while(true){
+            System.out.println("\n___INIZIO_GIOCO___");
 
-            System.out.println("\nsasso, carta, forbici: ");
-            choose = scanner.nextLine().toLowerCase();
+            while (true) {
 
-            if(isChoosen(choose)){
+                System.out.println("\nsasso, carta, forbici: ");
+                choose = scanner.nextLine().toLowerCase();
+
+                if (isChoosen(choose)) {
+
+                    break;
+                }
+
+                System.out.println("\nErrore: inserire una delle 3 mosse richieste");
+            }
+
+            ran = random.nextInt(3);
+            bot = botChoose(ran);
+
+            int result = theGame(choose, bot);
+
+            if (result == -1) {
+
+                System.out.println("\n__HAI PERSO T.T__");
+            } else if (result == 0) {
+                System.out.println("\n__PAREGGIO__");
+            } else {
+                System.out.println("__" + name + "... HAI VINTO!!!!!__");
+            }
+
+            System.out.println("Inserisci 'a' per chiudere");
+            String a = scanner.nextLine().toLowerCase();
+
+            if (a.equals("a")) {
 
                 break;
             }
 
-            System.out.println("Errore: inserire una delle 3 mosse richieste");
         }
 
-       
-        
-    
         scanner.close();
     }
 
-    //METODI
+    // METODI
 
-    public static boolean isAlpha(String x){
+    public static boolean isAlpha(String x) {
 
-        for(int i = 0;  i < x.length(); i++){
+        for (int i = 0; i < x.length(); i++) {
 
-            if(!Character.isLetter(x.charAt(i))){
+            if (!Character.isLetter(x.charAt(i))) {
 
                 return false;
             }
@@ -62,15 +86,43 @@ public class Script {
         return true;
     }
 
-    public static boolean isChoosen(String x){
+    public static boolean isChoosen(String x) {
 
-        if(x.equals("sasso") || x.equals("forbici") || x.equals("carta")){
+        if (x.equals("sasso") || x.equals("forbici") || x.equals("carta")) {
 
             return true;
         }
 
         return false;
 
+    }
+
+    public static String botChoose(int x) {
+
+        if (x == 0) {
+
+            return "sasso";
+        } else if (x == 1) {
+
+            return "carta";
+        } else {
+
+            return "forbici";
+        }
+    }
+
+    public static int theGame(String choose, String bot) {
+
+        if (choose.equals(bot)) {
+            return 0;
+        } else if ((choose.equals("carta") && bot.equals("sasso")) ||
+                (choose.equals("forbice") && bot.equals("carta")) ||
+                (choose.equals("sasso") && choose.equals("forbice"))) {
+            return 1;
+        } else {
+
+            return -1;
+        }
     }
 
 }
